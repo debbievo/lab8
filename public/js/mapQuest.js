d5d21a60dfa63ac58796d9b179e65c5547044e68
Debbie Vo <debbienvo@yahoo.com>
2019-02-27 13:21:04 -0800

    thirdpartyAPI lab

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,16 @@
 function initMap() {
 	// add your code here
+	L.mapquest.key = 'gxWLWA56jH40MeL8BGQhGhFFwMXQG1hd';
+
+	//map refers to a div element with the ID map
+	var map = L.mapquest.map('map', {
+		center:[32.8788, -117.2359],
+		layers: L.mapquest.tileLayer('map'),
+		zoom: 12,
+		zoomControl: false
+	});
+
+	//Add marker to map
+	//L.marker([32.88, -117.236]).addTo(map);
+	L.marker([32.8788, -117.2359]).addTo(map);
 }
--- routes/rsvp.js
+++ routes/rsvp.js
@@ -8,3 +8,16 @@ var data = {
 exports.adminView = function(request, response){
   response.render('rsvp', data);
 };
+
+
+exports.addRSVP = function(request, response){
+	var rsvpEmail = request.body.rsvpEmail;
+	//Print to terminal whenever post req made
+	console.log(rsvpEmail);
+
+	//Add to current data
+	data.rsvp.push(rsvpEmail);
+
+	//Send back data for browser to know it works
+	response.send(rsvpEmail);
+}
